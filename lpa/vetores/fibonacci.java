d5da777107db5a12a1ab1f29f4eadfe56e33af1f
Dantton Lavigne <vonkruschewsky@yandex.ru>
2025-05-08 10:53:01 -0300

    Update fibonacci.java

--- lpa/vetores/fibonacci.java
+++ lpa/vetores/fibonacci.java
@@ -1,5 +1,10 @@
 import java.util.Scanner;
 
+// Faça uma função que receba um número
+// inteiro N maior do que zero e preencha
+// um vetor com os N primeiros termos da
+// sequência de Fibonacci.
+
 public class fibonacci {
     public static void main(String[] args) {
         receberfibonacci();
