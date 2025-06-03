d186cf579311c0505073330bfa9a5ba49e3e9308
Dantton Lavigne <dantton.pinheiro@ucsal.edu.br>
2025-06-03 10:59:20 -0300

    Update jogoDaSenha.java

--- lpa/atividadePontuada/jogoDaSenha.java
+++ lpa/atividadePontuada/jogoDaSenha.java
@@ -1,123 +1,121 @@
-    package TESTE;
+import java.util.Random;
+import java.util.Scanner;
 
-    import java.util.Random;
-    import java.util.Scanner;
+public class jogoDaSenha {
 
-    public class jogoDaSenha {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        Random rand = new Random();
 
-        public static void main(String[] args) {
-            Scanner input = new Scanner(System.in);
-            Random rand = new Random();
+        boolean debug = true;
 
-            boolean debug = true;
+        int[] senha = new int[4];
+        int[] tentativa = new int[4];
 
-            int[] senha = new int[4];
-            int[] tentativa = new int[4];
+        gerarSenha(senha, rand);
 
-            gerarSenha(senha, rand);
+        System.out.println("=== Jogo da Senha ===");
+        System.out.println("A senha possui 4 dígitos, cada um de 1 a 6.");
+        System.out.println("Tente adivinhar a senha em até 10 tentativas!");
 
-            System.out.println("=== Jogo da Senha ===");
-            System.out.println("A senha possui 4 dígitos, cada um de 1 a 6.");
-            System.out.println("Tente adivinhar a senha em até 10 tentativas!");
+        if (debug) {
+            System.out.print("[DEBUG] Senha correta: ");
+            mostrarSenha(senha);
+        }
 
-            if (debug) {
-                System.out.print("[DEBUG] Senha correta: ");
-                mostrarSenha(senha);
-            }
+        boolean venceu = false;
+        int tentativas = 0;
 
-            boolean venceu = false;
-            int tentativas = 0;
+        while (tentativas < 10 && !venceu) {
+            tentativas++;
+            System.out.println("\nTentativa " + tentativas + ":");
+            System.out.print("Digite 4 números de 1 a 6, separados por espaço: ");
+            lerTentativa(tentativa, input);
 
-            while (tentativas < 10 && !venceu) {
-                tentativas++;
-                System.out.println("\nTentativa " + tentativas + ":");
-                System.out.print("Digite 4 números de 1 a 6, separados por espaço: ");
-                lerTentativa(tentativa, input);
+            int corretos = contarCorretos(senha, tentativa);
+            int deslocados = contarDeslocados(senha, tentativa);
 
-                int corretos = contarCorretos(senha, tentativa);
-                int deslocados = contarDeslocados(senha, tentativa);
+            System.out.println("Dígitos corretos (posição certa): " + corretos);
+            System.out.println("Dígitos corretos (posição errada): " + deslocados);
 
-                System.out.println("Dígitos corretos (posição certa): " + corretos);
-                System.out.println("Dígitos corretos (posição errada): " + deslocados);
-
-                if (corretos == 4) {
-                    venceu = true;
-                    System.out.println("\nParabéns! Você descobriu a senha em " + tentativas + " tentativas!");
-                }
-            }
-
-            if (!venceu) {
-                System.out.print("\nVocê perdeu! A senha correta era: ");
-                mostrarSenha(senha);
+            if (corretos == 4) {
+                venceu = true;
+                System.out.println("\nParabéns! Você descobriu a senha em " + tentativas + " tentativas!");
             }
         }
 
-        public static void gerarSenha(int[] senha, Random rand) {
-            for (int i = 0; i < senha.length; i++) {
-                senha[i] = rand.nextInt(6) + 1;
-            }
-        }
-
-        public static void lerTentativa(int[] tentativa, Scanner input) {
-            for (int i = 0; i < tentativa.length; i++) {
-                int valor = 0;
-                boolean valido = false;
-                while (!valido) {
-                    if (input.hasNextInt()) {
-                        valor = input.nextInt();
-                        if (valor >= 1 && valor <= 6) {
-                            valido = true;
-                        } else {
-                            System.out.print("Número fora do intervalo. Digite um número de 1 a 6: ");
-                        }
-                    } else {
-                        System.out.print("Entrada inválida. Digite um número de 1 a 6: ");
-                        input.next();
-                    }
-                }
-                tentativa[i] = valor;
-            }
-        }
-
-        public static int contarCorretos(int[] senha, int[] tentativa) {
-            int corretos = 0;
-            for (int i = 0; i < senha.length; i++) {
-                if (tentativa[i] == senha[i]) {
-                    corretos++;
-                }
-            }
-            return corretos;
-        }
-
-        public static int contarDeslocados(int[] senha, int[] tentativa) {
-            boolean[] senhaUsada = new boolean[senha.length];
-            boolean[] tentativaUsada = new boolean[senha.length];
-            int deslocados = 0;
-
-            for (int i = 0; i < senha.length; i++) {
-                if (tentativa[i] == senha[i]) {
-                    senhaUsada[i] = true;
-                    tentativaUsada[i] = true; 
-                }
-            }
-
-            for (int i = 0; i < tentativa.length; i++) {
-                if (!tentativaUsada[i]) { 
-                    for (int j = 0; j < senha.length; j++) { 
-                        if (!senhaUsada[j] && tentativa[i] == senha[j]) {
-                            deslocados++;
-                            senhaUsada[j] = true;
-                        }
-                    }
-                }                 
-            }
-            return deslocados;
-        }
-
-        public static void mostrarSenha(int[] senha) {
-            for (int i = 0; i < senha.length; i++) {
-                System.out.print(senha[i] + " ");
-            }
-            System.out.println();
+        if (!venceu) {
+            System.out.print("\nVocê perdeu! A senha correta era: ");
+            mostrarSenha(senha);
         }
     }
+
+    public static void gerarSenha(int[] senha, Random rand) {
+        for (int i = 0; i < senha.length; i++) {
+            senha[i] = rand.nextInt(6) + 1;
+        }
+    }
+
+    public static void lerTentativa(int[] tentativa, Scanner input) {
+        for (int i = 0; i < tentativa.length; i++) {
+            int valor = 0;
+            boolean valido = false;
+            while (!valido) {
+                if (input.hasNextInt()) {
+                    valor = input.nextInt();
+                    if (valor >= 1 && valor <= 6) {
+                        valido = true;
+                    } else {
+                        System.out.print("Número fora do intervalo. Digite um número de 1 a 6: ");
+                    }
+                } else {
+                    System.out.print("Entrada inválida. Digite um número de 1 a 6: ");
+                    input.next();
+                }
+            }
+            tentativa[i] = valor;
+        }
+    }
+
+    public static int contarCorretos(int[] senha, int[] tentativa) {
+        int corretos = 0;
+        for (int i = 0; i < senha.length; i++) {
+            if (tentativa[i] == senha[i]) {
+                corretos++;
+            }
+        }
+        return corretos;
+    }
+
+    public static int contarDeslocados(int[] senha, int[] tentativa) {
+        boolean[] senhaUsada = new boolean[senha.length];
+        boolean[] tentativaUsada = new boolean[senha.length];
+        int deslocados = 0;
+
+        for (int i = 0; i < senha.length; i++) {
+            if (tentativa[i] == senha[i]) {
+                senhaUsada[i] = true;
+                tentativaUsada[i] = true;
+            }
+        }
+
+        for (int i = 0; i < tentativa.length; i++) {
+            if (!tentativaUsada[i]) {
+                for (int j = 0; j < senha.length; j++) {
+                    if (!senhaUsada[j] && tentativa[i] == senha[j]) {
+                        deslocados++;
+                        senhaUsada[j] = true;
+                    }
+                }
+            }
+        }
+        return deslocados;
+    }
+
+    public static void mostrarSenha(int[] senha) {
+        for (int i = 0; i < senha.length; i++) {
+            System.out.print(senha[i] + " ");
+        }
+        System.out.println();
+    }
+}
